c5f012d22e52cc04808e4e24b2cc458f5b78ad99
engallote <53823323+engallote@users.noreply.github.com>
2020-02-06 01:11:04 +0900

    Update 2239.java

--- 2239.java
+++ 2239.java
@@ -7,11 +7,9 @@ public class Main {
 	static ArrayList<Pair> aly = new ArrayList<>();
 	public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        for(int i = 0; i < 9; i++)
-        {
+        for(int i = 0; i < 9; i++){
         	char[] c = sc.next().toCharArray();
-        	for(int j = 0; j < 9; j++)
-        	{
+        	for(int j = 0; j < 9; j++){
         		arr[i][j] = c[j] - '0';
         		if(arr[i][j] == 0)
         			aly.add(new Pair(i, j));
@@ -22,11 +20,9 @@ public class Main {
     }
 	private static void dfs(int idx) {
 		if(flag) return;
-		if(idx == aly.size())
-		{
+		if(idx == aly.size()){
 			flag = true;
-			for(int i = 0; i < 9; i++)
-			{
+			for(int i = 0; i < 9; i++){
 				for(int j = 0; j < 9; j++)
 					System.out.print(arr[i][j]);
 				System.out.println();
@@ -34,132 +30,24 @@ public class Main {
 			return;
 		}
 		
-		for(int i = 1; i <= 9; i++)
-		{
+		for(int i = 1; i <= 9; i++){
 			int x = aly.get(idx).x;
 			int y = aly.get(idx).y;
-			arr[x][y] = i;
-			if(sudoku(x, y))
-				dfs(idx + 1);	
-			arr[x][y] = 0;
+			if(sudoku(x, y, i)){
+				arr[x][y] = i;
+				dfs(idx + 1);
+				arr[x][y] = 0;
+			}
 		}
 	}
-	private static boolean sudoku(int x, int y) {
-		HashSet<Integer> hs = new HashSet<>();
-		for(int i = 0; i < 9; i++)
-		{
-			if(arr[x][i] == 0) continue;
-			if(hs.contains(arr[x][i])) return false;
-			hs.add(arr[x][i]);
-		}
-		hs.clear();
-		for(int i = 0; i < 9; i++)
-		{
-			if(arr[i][y] == 0) continue;
-			if(hs.contains(arr[i][y])) return false;
-			hs.add(arr[i][y]);
-		}
+	private static boolean sudoku(int x, int y, int num) {
+		for(int i = 0; i < 9; i++)//세로 가로 확인
+			if(arr[x][i] == num || arr[i][y] == num) return false;
 		
-		hs.clear();
-		if(x >= 0 && x < 3)
-		{
-			if(y >= 0 && y < 3)
-			{
-				for(int i = 0; i < 3; i++)
-					for(int j = 0; j < 3; j++)
-					{
-						if(arr[i][j] == 0) continue;
-						if(hs.contains(arr[i][j])) return false;
-						hs.add(arr[i][j]);
-					}
-			}
-			else if(y >= 3 && y < 6)
-			{
-				for(int i = 0; i < 3; i++)
-					for(int j = 3; j < 6; j++)
-					{
-						if(arr[i][j] == 0) continue;
-						if(hs.contains(arr[i][j])) return false;
-						hs.add(arr[i][j]);
-					}
-			}
-			else
-			{
-				for(int i = 0; i < 3; i++)
-					for(int j = 6; j < 9; j++)
-					{
-						if(arr[i][j] == 0) continue;
-						if(hs.contains(arr[i][j])) return false;
-						hs.add(arr[i][j]);
-					}
-			}
-		}
-		else if(x >= 3 && x < 6)
-		{
-			if(y >= 0 && y < 3)
-			{
-				for(int i = 3; i < 6; i++)
-					for(int j = 0; j < 3; j++)
-					{
-						if(arr[i][j] == 0) continue;
-						if(hs.contains(arr[i][j])) return false;
-						hs.add(arr[i][j]);
-					}
-			}
-			else if(y >= 3 && y < 6)
-			{
-				for(int i = 3; i < 6; i++)
-					for(int j = 3; j < 6; j++)
-					{
-						if(arr[i][j] == 0) continue;
-						if(hs.contains(arr[i][j])) return false;
-						hs.add(arr[i][j]);
-					}
-			}
-			else
-			{
-				for(int i = 3; i < 6; i++)
-					for(int j = 6; j < 9; j++)
-					{
-						if(arr[i][j] == 0) continue;
-						if(hs.contains(arr[i][j])) return false;
-						hs.add(arr[i][j]);
-					}
-			}
-		}
-		else
-		{
-			if(y >= 0 && y < 3)
-			{
-				for(int i = 6; i < 9; i++)
-					for(int j = 0; j < 3; j++)
-					{
-						if(arr[i][j] == 0) continue;
-						if(hs.contains(arr[i][j])) return false;
-						hs.add(arr[i][j]);
-					}
-			}
-			else if(y >= 3 && y < 6)
-			{
-				for(int i = 6; i < 9; i++)
-					for(int j = 3; j < 6; j++)
-					{
-						if(arr[i][j] == 0) continue;
-						if(hs.contains(arr[i][j])) return false;
-						hs.add(arr[i][j]);
-					}
-			}
-			else
-			{
-				for(int i = 6; i < 9; i++)
-					for(int j = 6; j < 9; j++)
-					{
-						if(arr[i][j] == 0) continue;
-						if(hs.contains(arr[i][j])) return false;
-						hs.add(arr[i][j]);
-					}
-			}
-		}
+		int xx = x / 3 * 3, yy = y / 3 * 3;//구역 확인
+		for(int i = xx; i < xx + 3; i++)
+			for(int j = yy; j < yy + 3; j++)
+				if(arr[i][j] == num) return false;
 		return true;
 	}
 }
