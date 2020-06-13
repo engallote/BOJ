7d090050ea92dee822cda6d826ff4c166bb19690
engallote <53823323+engallote@users.noreply.github.com>
2020-06-13 22:16:53 +0900

    Update 2579.java

--- 2579.java
+++ 2579.java
@@ -9,15 +9,19 @@ public class Main {
 		for(int i = 0; i < N; i++)
 			arr[i] = sc.nextInt();
 		
-		int[] dp = new int[N];
+        	if(N == 1) System.out.println(arr[0]);
+        	else if(N == 2) System.out.println(arr[0] + arr[1]);
+        	else{
+            		int[] dp = new int[N];
 		
-		dp[0] = arr[0];
-		dp[1] = arr[0] + arr[1];
-		dp[2] = Math.max(arr[0], arr[1]) + arr[2];
+		    	dp[0] = arr[0];
+		    	dp[1] = arr[0] + arr[1];
+		    	dp[2] = Math.max(arr[0], arr[1]) + arr[2];
 		
-		for(int i = 3; i < N; i++)
-			dp[i] = Math.max(dp[i-3] + arr[i-1] + arr[i], dp[i-2] + arr[i]);
+		    	for(int i = 3; i < N; i++)
+			    dp[i] = Math.max(dp[i-3] + arr[i-1] + arr[i], dp[i-2] + arr[i]);
 		
-		System.out.println(dp[N-1]);
+		    	System.out.println(dp[N-1]);
+        	}
 	}
 }
