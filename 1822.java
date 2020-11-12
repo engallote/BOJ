798cacec180205846a502920efd862ef98d6c467
engallote <53823323+engallote@users.noreply.github.com>
2020-11-12 23:58:05 +0900

    Update 1822.java

--- 1822.java
+++ 1822.java
@@ -1,29 +1,37 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.*;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		int M = sc.nextInt();
-		HashSet<Long> hs = new HashSet<>();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
 		
+		HashSet<String> hs = new HashSet<>();
+		PriorityQueue<Integer> pq = new PriorityQueue<>();
+		st = new StringTokenizer(br.readLine());
 		for(int i = 0; i < N; i++)
-			hs.add(sc.nextLong());
+			hs.add(st.nextToken());
 		
-		for(int i = 0; i < M; i++)
-		{
-			long num = sc.nextLong();
-			if(hs.contains(num))
-				hs.remove(num);
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < M; i++) {
+			String str = st.nextToken();
+			if(hs.contains(str)) hs.remove(str);
 		}
 		
 		System.out.println(hs.size());
-		if(hs.size() != 0)
-		{
-			Object[] arr = hs.toArray();
-			Arrays.sort(arr);
-			for(Object a : arr)
-				System.out.print(a + " ");
-		}
+		Iterator<String> it = hs.iterator();
+		while(it.hasNext())
+			pq.offer(Integer.parseInt(it.next()));
+		
+		while(!pq.isEmpty())
+			bw.write(pq.poll() + " ");
+		bw.flush();
 	}
 }
